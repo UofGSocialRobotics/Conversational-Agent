8295717557028352b42e1d0800aff991894eb46a
Florian Pecune <florian.pecune@gmail.com>
2019-05-29 23:13:45 +0100

    added user model and recommendations taking that into account

--- NLG.py
+++ NLG.py
@@ -2,6 +2,7 @@ import whiteboard_client as wbc
 import helper_functions as helper
 import json
 import random
+import config
 
 class NLG(wbc.WhiteBoardClient):
     def __init__(self, subscribes, publishes, clientid):
@@ -9,11 +10,17 @@ class NLG(wbc.WhiteBoardClient):
         publishes = publishes + clientid
         wbc.WhiteBoardClient.__init__(self, "NLG"+clientid, subscribes, publishes)
         self.sentenceDB = {}
-        self.load_model("./resources/nlg/sentence_db.csv")
+        self.ackDB = {}
 
-        self.movie = ""
+        self.movie_title = ""
 
-    def load_model(self, path):
+        self.load_sentence_model(config.NLG_SENTENCE_DB)
+        self.load_ack_model(config.NLG_ACK_DB)
+
+        # Do we generate acknowledgement?
+        self.use_acks = False
+
+    def load_sentence_model(self, path):
         with open(path) as f:
             for line in f:
                 line_input = line.split(",")
@@ -22,20 +29,41 @@ class NLG(wbc.WhiteBoardClient):
                 else:
                     self.sentenceDB[line_input[0]] = [line_input[2]]
 
+    def load_ack_model(self, path):
+        with open(path) as f:
+            for line in f:
+                line_input = line.split(",")
+                if self.ackDB.get(line_input[0]) is not None:
+                    self.ackDB.get(line_input[0]).append(line_input[2])
+                else:
+                    self.ackDB[line_input[0]] = [line_input[2]]
+
     def treat_message(self, msg, topic):
         message = json.loads(msg)
         sentence = random.choice(self.sentenceDB[message['intent']])
-        self.movie = message['movies']
-        sentence = self.set_movie_tile_in_sentence(sentence)
-        self.publish(sentence)
+        self.movie_title = message['movies']['title']
+        if self.use_acks:
+            ack = random.choice(self.ackDB[message['intent']])
+        else:
+            ack = ""
+        final_sentence = self.replace(ack + " " + sentence)
+        self.publish(final_sentence)
 
-    def set_movie_tile_in_sentence(self, sentence):
+        # Todo Add acks more smartly
+
+        # Todo Add explanations
+
+    def replace(self, sentence):
+        # Todo: - Replace movie info (#plot, #actors, #genres, etc...)
+        #  - Replace adjency pair (#actor, #director, #genre)
         if "#title" in sentence:
             # movListString = ""
             # for mov in self.moviesList:
             #     movListString = movListString + " " + mov
-            sentence = sentence.replace("#title", self.movie.replace("?", "e"))
+            sentence = sentence.replace("#title", self.movie_title.replace("?", "e"))
         return sentence
+
+
 
 if __name__ == "__main__":
     fake_msg = {'intent': 'inform(movie)', 'movies': 'John Wick: Chapter 3 – Parabellum'}
--- config.py
+++ config.py
@@ -71,3 +71,19 @@ import dum_sentiment_analysis
 SentimentAnalysis = dum_sentiment_analysis.SentimentAnalysis
 SentimentAnalysis_subscribes = [MSG_SERVER_IN]
 SentimentAnalysis_publishes = MSG_SA
+
+
+####################################################################################################
+##                                          Resources Path                                        ##
+####################################################################################################
+
+DM_MODEL = "./resources/dm/Model.csv"
+NLG_SENTENCE_DB = "./resources/nlg/sentence_db.csv"
+NLG_ACK_DB = "./resources/nlg/ack_db.csv"
+
+MOVIEDB_KEY = "6e3c2d4a2501c86cd7e0571ada291f55"
+MOVIEDB_SEARCH_MOVIE_ADDRESS = "https://api.themoviedb.org/3/discover/movie?api_key="
+MOVIE_DB_PROPERTY = "&sort_by=popularity.desc"
+MOVIEDB_SEARCH_PERSON_ADDRESS = "https://api.themoviedb.org/3/search/person?api_key="
+OMDB_SEARCH_MOVIE_INFO = "http://www.omdbapi.com/?t="
+OMDB_KEY = "be72fd68"
