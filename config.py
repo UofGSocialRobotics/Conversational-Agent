76d500683851c6298a72cef4c52859a67b50e458
LucileC <lucile.callebert@gmail.com>
2019-06-19 12:05:35 +0100

    clean config: splited into general and domain specific

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import movies
 import food
-from movies import NLG, rule_based_NLU, dum_sentiment_analysis
-from food import DM
 
 ####################################################################################################
 ##                                Using broker or websockets on localhost                         ##
@@ -63,82 +61,23 @@ MSG_CONFIRM_CONNECTION = "Connection confirmed"
 ## NLU
 #import dum_NLU
 #NLU = dum_NLU.NLU
-NLU = movies.rule_based_NLU.RuleBasedNLU
+NLU = None
 NLU_subscribes = [MSG_SERVER_IN]
 NLU_publishes = MSG_NLU
 
 ## DM
-DM = food.DM.DM
+DM = None
 DM_subscribes = [MSG_NLU, MSG_SA]
 DM_publishes = MSG_DM
 
 ## NLG
-NLG = movies.NLG.NLG
+NLG = None
 NLG_subscribes = [MSG_DM]
 NLG_publishes = MSG_NLG
 
 ## Sentiment analysis
-SentimentAnalysis = movies.dum_sentiment_analysis.SentimentAnalysis
+SentimentAnalysis = None
 SentimentAnalysis_subscribes = [MSG_SERVER_IN]
 SentimentAnalysis_publishes = MSG_SA
 
 
-####################################################################################################
-##                                          Resources Path                                        ##
-####################################################################################################
-
-DM_MODEL = "./movies/resources/dm/Model.csv"
-USER_MODELS = "./movies/resources/user_models/"
-NLG_SENTENCE_DB = "./movies/resources/nlg/sentence_db.csv"
-NLG_ACK_DB = "./movies/resources/nlg/ack_db.csv"
-ACTORS_LEXICON = "./movies/resources/nlu/actor2id.lexicon"
-DIRECTORS_LEXICON = "./movies/resources/nlu/director2id.lexicon"
-GENRES_LEXICON = "./movies/resources/nlu/genre2id.lexicon"
-
-
-####################################################################################################
-##                                      Movie recommendation config                               ##
-####################################################################################################
-MOVIEDB_KEY = "6e3c2d4a2501c86cd7e0571ada291f55"
-MOVIEDB_SEARCH_MOVIE_ADDRESS = "https://api.themoviedb.org/3/discover/movie?api_key="
-MOVIE_DB_PROPERTY = "&sort_by=popularity.desc"
-MOVIEDB_SEARCH_PERSON_ADDRESS = "https://api.themoviedb.org/3/search/person?api_key="
-MOVIEDB_POSTER_PATH = "https://image.tmdb.org/t/p/original/"
-OMDB_SEARCH_MOVIE_INFO = "http://www.omdbapi.com/?t="
-OMDB_KEY = "be72fd68"
-HIGH_QUALITY_POSTER = True
-
-####################################################################################################
-##                                          Other config                                          ##
-####################################################################################################
-SAVE_USER_MODEL = False
-NLG_USE_ACKS = True
-NLG_USE_ACKS_CS = True
-NLG_USE_CS = True
-NLG_USE_EXPLANATIONS = True
-CS_LABELS = ["SD", "PR", "HE", "VSN", "NONE", 'QESD']
-EXPLANATION_TYPE_LABELS = ["MF", "TPO", "PO", "PE"]
-EXPLANATION_TYPE_PROBA = [.37, .07, .39, .17]
-MF_EXPLANATION_LABELS = ["C", "G", "P", "A", "O"]
-MF_EXPLANATION_PROBA = [.108, .243, .459, .055, .135]
-TPO_EXPLANATION_LABELS = ["B", "S"]
-TPO_EXPLANATION_PROBA = [.571, .429]
-PO_EXPLANATION_LABELS = ["POS", "ANA", "SO"]
-PO_EXPLANATION_PROBA = [.513, .308, .179]
-PE_EXPLANATION_LABELS = ["A", "L", "C"]
-PE_EXPLANATION_PROBA = [.177, .529, .294]
-
-
-
-
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-##########                                      FOOD PROJECT                            #############
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-
-FOOD_MODEL_PATH = "./food/resources/dm/food_ratings.csv"
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from ca_logging import log
 import config
+import argparse
 
-log.info("Starting system")
+def main():
+    log.info("Starting system")
 
-if config.USING == config.BROKER:
-    import server_using_borker
-    log.info("Will use broker")
-    server = server_using_borker.ServerUsingBroker()
-elif config.USING == config.WEBSOCKETS:
-    log.info("Will use websockets")
-    import server_using_websockets
-    server = server_using_websockets.ServerUsingWebSockets()
-else:
-    log.warning("Wrong server config")
-    exit(0)
+    if config.USING == config.BROKER:
+        import server_using_borker
+        log.info("Will use broker")
+        server = server_using_borker.ServerUsingBroker()
+    elif config.USING == config.WEBSOCKETS:
+        log.info("Will use websockets")
+        import server_using_websockets
+        server = server_using_websockets.ServerUsingWebSockets()
+    else:
+        log.warning("Wrong server config")
+        exit(0)
 
-server.start_service()
+    server.start_service()
+
+
+if __name__ == '__main__':
+
+    argp = argparse.ArgumentParser()
+    argp.add_argument("--food", help="Movie recommandation system", action="store_true")
+    argp.add_argument("--movies", help="Food derommandation system", action="store_true")
+    # argp.add_argument("--debug", help="Sentence to debug", action="store")
+
+    args = argp.parse_args()
+
+    if args.movies:
+        import movies.movie_config as movie_config
+        config.NLU = movie_config.NLU
+        config.DM = movie_config.DM
+        config.NLG = movie_config.NLG
+        config.SentimentAnalysis = movie_config.SentimentAnalysis
+        main()
 
