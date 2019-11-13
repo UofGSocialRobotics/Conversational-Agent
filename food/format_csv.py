cc8abc66de6d204fe857fa7d4a4a9a5fdbd8ca04
LucileC <lucile.callebert@gmail.com>
2019-11-13 11:41:09 +0000

    added without as a NO word

--- food/format_csv.py
+++ food/format_csv.py
@@ -1,95 +1,107 @@
 import pandas
+import argparse
 
-csv_in_path = "food/resources/pilot3/data.csv"
+def format_data(path_read_from, path_write_to):
 
-df = pandas.read_csv(csv_in_path)
-# print(df)
+    df = pandas.read_csv(path_read_from)
+    # print(df)
 
-prolific_id, cond_str = "prolific_id", "XP_condition"
-esthers_q = ("question17", "question18", "question19", "question20", "question21")
-rapport_q = ("question1", "question2", "question3", "question4", "question5", "question6", "question7", "question8_reversed")
-task_perf_q = ("question9", "question10", "question11", "question12", "question13", "question14", "question15", "question16")
-questions_list = [esthers_q, rapport_q, task_perf_q]
-files_names = {esthers_q: "intiont_to_cook", rapport_q: "rapport", task_perf_q: "task performance"}
-conditions = ["NONE", "robot", "human"]
+    prolific_id, cond_str = "prolific_id", "XP_condition"
+    esthers_q = ("q17", "q18", "q19", "q20", "q21")
+    rapport_q = ("q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8_r")
+    task_perf_q = ("q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16")
+    questions_list = [esthers_q, rapport_q, task_perf_q]
+    files_names = {esthers_q: "intiont_to_cook", rapport_q: "rapport", task_perf_q: "task performance"}
+    conditions = ["NONE", "robot", "human"]
 
-for _q in questions_list:
-    cols = [prolific_id, cond_str] + list(_q)
-    # print(cols)
-    df_q_all = df[cols]
+    for _q in questions_list:
+        cols = [prolific_id, cond_str] + list(_q)
+        print(cols)
+        # print(cols)
+        df_q_all = df[cols]
 
-    n_participants_per_cond = dict()
-    df_by_cond = dict()
-    max_participants = 0
+        n_participants_per_cond = dict()
+        df_by_cond = dict()
+        max_participants = 0
 
-    for cond in conditions:
-        df_by_cond[cond] = df_q_all.query("XP_condition == '" + cond + "'")
-        tmp_list = df_by_cond[cond][_q[0]].to_list()
-        tmp_series = pandas.Series(tmp_list)
-        tmp_series = tmp_series[~tmp_series.isnull()]
-        n_participants_per_cond[cond] = len(tmp_series.to_list())
-        if n_participants_per_cond[cond] > max_participants:
-            max_participants = n_participants_per_cond[cond]
-    # print("paticipants", cond, n_participants_per_cond[cond])
-
-
-    new_df = pandas.DataFrame(None, columns=['prolific_id'] + conditions)
-    _scores_per_cond = dict()
-    for cond in conditions:
-        # df_q_by_cond = df_q_all.query("XP_condition == '" + cond + "'")
-        _scores = list()
-        prolific_ids = list()
-        for num_q, question in enumerate(_q):
-            if num_q == 0:
-                tmp_average = df_by_cond[cond].sum(axis=1, skipna=False).to_list()
-                # print(tmp_average)
-            prolific_ids += df_by_cond[cond][prolific_id].to_list()
-            tmp_list = df_by_cond[cond][question].to_list()
+        for cond in conditions:
+            df_by_cond[cond] = df_q_all.query("XP_condition == '" + cond + "'")
+            tmp_list = df_by_cond[cond][_q[0]].to_list()
             tmp_series = pandas.Series(tmp_list)
-            # tmp_series = tmp_series[~tmp_series.isnull()]
-            # tmp_list = tmp_series.to_list()
-            for i, elt in enumerate(tmp_series):
-                if not pandas.isnull(elt):
-                    p_id = prolific_ids[i]
-                    if num_q == 0:
-                        average = tmp_average[i] / len(_q)
-                    else:
-                        average = ""
+            tmp_series = tmp_series[~tmp_series.isnull()]
+            n_participants_per_cond[cond] = len(tmp_series.to_list())
+            if n_participants_per_cond[cond] > max_participants:
+                max_participants = n_participants_per_cond[cond]
+        # print("paticipants", cond, n_participants_per_cond[cond])
 
-                    _scores.append([p_id, question, elt, average])
-        while (len(_scores) < max_participants * len(_q)):
-            _scores.append(["", "", "", ""])
 
-        _scores_per_cond[cond] = _scores
-        # print()
+        new_df = pandas.DataFrame(None, columns=['prolific_id'] + conditions)
+        _scores_per_cond = dict()
+        for cond in conditions:
+            # df_q_by_cond = df_q_all.query("XP_condition == '" + cond + "'")
+            _scores = list()
+            prolific_ids = list()
+            for num_q, question in enumerate(_q):
+                if num_q == 0:
+                    tmp_average = df_by_cond[cond].sum(axis=1, skipna=False).to_list()
+                    # print(tmp_average)
+                prolific_ids += df_by_cond[cond][prolific_id].to_list()
+                tmp_list = df_by_cond[cond][question].to_list()
+                tmp_series = pandas.Series(tmp_list)
+                # tmp_series = tmp_series[~tmp_series.isnull()]
+                # tmp_list = tmp_series.to_list()
+                for i, elt in enumerate(tmp_series):
+                    if not pandas.isnull(elt):
+                        p_id = prolific_ids[i]
+                        if num_q == 0:
+                            average = tmp_average[i] / len(_q)
+                        else:
+                            average = ""
 
-    # print(_scores_per_cond)
-    new_df_list = list()
-    columns_list = list()
+                        _scores.append([p_id, question, elt, average])
+            while (len(_scores) < max_participants * len(_q)):
+                _scores.append(["", "", "", ""])
 
-    first_cond_bool = True
-    for cond, scores in _scores_per_cond.items():
-        columns_list += [c + "_" + cond for c in [prolific_id, "question", "score", "average_over_questions"]]
-        for i, s in enumerate(scores):
-            if first_cond_bool:
-                new_df_list.append(s)
-            else:
-                new_df_list[i] += s
-        first_cond_bool = False
+            _scores_per_cond[cond] = _scores
+            # print()
 
-    print(len(new_df_list))
-    new_df = pandas.DataFrame(new_df_list, columns=columns_list)
-    # print(new_df)
+        # print(_scores_per_cond)
+        new_df_list = list()
+        columns_list = list()
 
-    ordered_cols = list()
-    for i in [prolific_id, "question", "score", "average_over_questions"]:
-        for j in ["_" + c for c in conditions]:
-            ordered_cols.append(i + j)
-    print("ordered_cols", ordered_cols)
+        first_cond_bool = True
+        for cond, scores in _scores_per_cond.items():
+            columns_list += [c + "_" + cond for c in [prolific_id, "question", "score", "average_over_questions"]]
+            for i, s in enumerate(scores):
+                if first_cond_bool:
+                    new_df_list.append(s)
+                else:
+                    new_df_list[i] += s
+            first_cond_bool = False
 
-    # print(new_df)
-    new_df = new_df[ordered_cols]
-    # new_df = pandas.DataFrame(_scores, columns=[prolific_id, "question", "score", "average_over_questions"])
-    new_df.to_csv("food/resources/pilot3/"+files_names[_q]+".csv")
+        print(len(new_df_list))
+        new_df = pandas.DataFrame(new_df_list, columns=columns_list)
+        # print(new_df)
 
-    # print(new_df)
+        ordered_cols = list()
+        for i in [prolific_id, "question", "score", "average_over_questions"]:
+            for j in ["_" + c for c in conditions]:
+                ordered_cols.append(i + j)
+        print("ordered_cols", ordered_cols)
+
+        # print(new_df)
+        new_df = new_df[ordered_cols]
+        # new_df = pandas.DataFrame(_scores, columns=[prolific_id, "question", "score", "average_over_questions"])
+        new_df.to_csv(path_write_to+files_names[_q]+".csv")
+
+        # print(new_df)
+
+if __name__ == "__main__":
+
+    argp = argparse.ArgumentParser()
+    argp.add_argument('data_folder', metavar='data_folder', type=str, help='Where to get the data from? and write the data to?')
+
+    args = argp.parse_args()
+    path_read_from = "food/resources/"+args.data_folder+"/data.csv"
+    path_write_to = "food/resources/"+args.data_folder+"/"
+    format_data(path_read_from, path_write_to)
--- food/json_to_csv.py
+++ food/json_to_csv.py
@@ -1,9 +1,8 @@
 import csv
 import json
 import datetime
+import argparse
 
-path = "food/resources/pilot3/data.json"
-csv_path = "food/resources/pilot3/data.csv"
 
 def format_date_splited_at_separator(date_val, separator):
     date_splited_at_slash = date_val.split(separator)
@@ -55,109 +54,118 @@ def parse_datetime(value):
             print(len("09/11/2019,14:45:33"), len("11/9/2019,10:12:06AM"), len(new_value))
             raise "error"
 
-with open(path, 'r') as f:
-    with open(csv_path, 'w') as fcsv:
-        csv_writer = csv.writer(fcsv)
+def json_to_csv(path_read_from, path_write_to):
+    with open(path_read_from, 'r') as f:
+        with open(path_write_to+"data.csv", 'w') as fcsv:
+            csv_writer = csv.writer(fcsv)
 
-        content = json.load(f)
-        sessions = content["Sessions"]
-        first_line = list()
-        first_line_bool = True
-        for id, data in sessions.items():
+            content = json.load(f)
+            sessions = content["Sessions"]
+            first_line = list()
+            first_line_bool = True
+            for id, data in sessions.items():
 
-            if isinstance(data, dict):
-                # print(data)
-                # print("\n-----\n")
-                if first_line_bool:
-                    first_line.append("id")
-                # try:
+                if isinstance(data, dict):
                     # print(data)
-                data_participant = list()
-                if data["data_collection"] and "amt_id" in data["data_collection"].keys() and data["data_collection"]["amt_id"]:
-                    prolific_id = data["data_collection"]["amt_id"]["value"]
-                    data_participant.append(prolific_id)
+                    # print("\n-----\n")
                     if first_line_bool:
-                        first_line.append("prolific_id")
-                start_time = False
-                stop_time = False
-                if "xp_condition" in data.keys():
-                    data_participant.append(data["xp_condition"])
-                    if first_line_bool:
-                        first_line.append("XP_condition")
-                diaglog = list()
-                if data["data_collection"]["amt_id"]:
-                    for key, value in data["data_collection"]["amt_id"].items():
-                        if key == "datetime" and not start_time:
-                            start_time = parse_datetime(value)
-                if "demographics" in data["data_collection"].keys() and data["data_collection"]["demographics"]:
-                    for key, value in data["data_collection"]["demographics"].items():
-                        if key != "client_id" and key != "datetime" and not any(w in key for w in ["feet", "pounds", "stones", 'height', "weight"]):
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                if "food_diagnosis_answers" in data["data_collection"].keys() and data["data_collection"]["food_diagnosis_answers"]:
-                    for key, value in data["data_collection"]["food_diagnosis_answers"].items():
-                        if key != "client_id" and key != "datetime":
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                if "questionnaire_answers_q1" in data["data_collection"].keys() and data["data_collection"]["questionnaire_answers_q1"]:
-                    for key, value in data["data_collection"]["questionnaire_answers_q1"].items():
-                        if key != "client_id" and key != "datetime":
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                            if key == "question8":
-                                data_participant.append(6-int(value))
+                        first_line.append("id")
+                    # try:
+                        # print(data)
+                    data_participant = list()
+                    if data["data_collection"] and "amt_id" in data["data_collection"].keys() and data["data_collection"]["amt_id"]:
+                        prolific_id = data["data_collection"]["amt_id"]["value"]
+                        data_participant.append(prolific_id)
+                        if first_line_bool:
+                            first_line.append("prolific_id")
+                    start_time = False
+                    stop_time = False
+                    if "xp_condition" in data.keys():
+                        data_participant.append(data["xp_condition"])
+                        if first_line_bool:
+                            first_line.append("XP_condition")
+                    diaglog = list()
+                    if data["data_collection"]["amt_id"]:
+                        for key, value in data["data_collection"]["amt_id"].items():
+                            if key == "datetime" and not start_time:
+                                start_time = parse_datetime(value)
+                    if "demographics" in data["data_collection"].keys() and data["data_collection"]["demographics"]:
+                        for key, value in data["data_collection"]["demographics"].items():
+                            if key != "client_id" and key != "datetime" and not any(w in key for w in ["feet", "pounds", "stones", 'height', "weight"]):
+                                data_participant.append(value)
                                 if first_line_bool:
-                                    first_line.append("question8_reversed")
-                if "questionnaire_answers_q2" in data["data_collection"].keys() and data["data_collection"]["questionnaire_answers_q2"]:
-                    for key, value in data["data_collection"]["questionnaire_answers_q2"].items():
-                        if key != "client_id" and key != "datetime":
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                if "data_recommendation" in data.keys():
-                    for key, value in data["data_recommendation"].items():
-                        if key != "client_id" and key != "datetime":
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                if "free_comments" in data["data_collection"].keys() and data["data_collection"]["free_comments"]:
-                    for key, value in data["data_collection"]["free_comments"].items():
-                        if key != "client_id" and key != "datetime":
-                            data_participant.append(value)
-                            if first_line_bool:
-                                first_line.append(key)
-                        if key == "datetime" and not stop_time:
-                            if first_line_bool:
-                                first_line.append("duration")
-                            stop_time = parse_datetime(value)
-                            if start_time and stop_time:
-                                data_participant.append(stop_time - start_time)
+                                    first_line.append(key)
+                    if "food_diagnosis_answers" in data["data_collection"].keys() and data["data_collection"]["food_diagnosis_answers"]:
+                        for key, value in data["data_collection"]["food_diagnosis_answers"].items():
+                            if key != "client_id" and key != "datetime":
+                                data_participant.append(value)
+                                if first_line_bool:
+                                    first_line.append('fq'+key[len("question"):])
+                    if "questionnaire_answers_q1" in data["data_collection"].keys() and data["data_collection"]["questionnaire_answers_q1"]:
+                        for key, value in data["data_collection"]["questionnaire_answers_q1"].items():
+                            if key != "client_id" and key != "datetime":
+                                data_participant.append(value)
+                                if first_line_bool:
+                                    first_line.append('q'+key[len("question"):])
+                                if key == "question8":
+                                    data_participant.append(6-int(value))
+                                    if first_line_bool:
+                                        first_line.append("q8_r")
+                    if "questionnaire_answers_q2" in data["data_collection"].keys() and data["data_collection"]["questionnaire_answers_q2"]:
+                        for key, value in data["data_collection"]["questionnaire_answers_q2"].items():
+                            if key != "client_id" and key != "datetime":
+                                data_participant.append(value)
+                                if first_line_bool:
+                                    first_line.append('q'+key[len("question"):])
+                    if "data_recommendation" in data.keys():
+                        for key, value in data["data_recommendation"].items():
+                            if key != "client_id" and key != "datetime":
+                                data_participant.append(value)
+                                if first_line_bool:
+                                    first_line.append(key)
+                    if "free_comments" in data["data_collection"].keys() and data["data_collection"]["free_comments"]:
+                        for key, value in data["data_collection"]["free_comments"].items():
+                            if key != "client_id" and key != "datetime":
+                                data_participant.append(value)
+                                if first_line_bool:
+                                    first_line.append(key)
+                            if key == "datetime" and not stop_time:
+                                if first_line_bool:
+                                    first_line.append("duration")
+                                stop_time = parse_datetime(value)
+                                if start_time and stop_time:
+                                    data_participant.append(stop_time - start_time)
 
-                if data["dialog"]:
-                    # print(data["dialog"],"\n\n")
-                    for data_dialog in data["dialog"].values():
-                        if data_dialog:
-                            if "text" in data_dialog.keys():
-                                utterance = data_dialog['text']
-                            elif "sentence" in data_dialog.keys():
-                                utterance = data_dialog['sentence']
-                            diaglog.append(utterance)
+                    if data["dialog"]:
+                        # print(data["dialog"],"\n\n")
+                        for data_dialog in data["dialog"].values():
+                            if data_dialog:
+                                if "text" in data_dialog.keys():
+                                    utterance = data_dialog['text']
+                                elif "sentence" in data_dialog.keys():
+                                    utterance = data_dialog['sentence']
+                                diaglog.append(utterance)
 
-                # except Error:
-                #     print(data)
+                    # except Error:
+                    #     print(data)
 
-                if first_line_bool:
-                    csv_writer.writerow(first_line)
-                    first_line_bool = False
-                participant_data = [id[1:]] + data_participant
-                csv_writer.writerow(participant_data)
+                    old_data_participant = data_participant
+                    data_participant = list()
+                    for piece_of_data in old_data_participant:
+                        if isinstance(piece_of_data, str):
+                            data_participant.append(" ".join(piece_of_data.split("\n")))
+                        else:
+                            data_participant.append(piece_of_data)
 
-                with open('food/resources/pilot2/'+prolific_id+".txt", 'w+') as dialog_file:
-                    for line in diaglog:
-                        dialog_file.write(line+'\n')
+                    if first_line_bool:
+                        csv_writer.writerow(first_line)
+                        first_line_bool = False
+                    participant_data = [id[1:]] + data_participant
+                    csv_writer.writerow(participant_data)
+
+                    with open(path_write_to+prolific_id+".txt", 'w+') as dialog_file:
+                        for line in diaglog:
+                            dialog_file.write(line+'\n')
 
 
 
@@ -165,6 +173,13 @@ with open(path, 'r') as f:
 
 
 
+if __name__ == "__main__":
 
+    argp = argparse.ArgumentParser()
+    argp.add_argument('data_folder', metavar='data_folder', type=str, help='Where to get the data from? and write the data to?')
 
+    args = argp.parse_args()
+    path_read_from = "food/resources/"+args.data_folder+"/data.json"
+    path_write_to = "food/resources/"+args.data_folder+"/"
+    json_to_csv(path_read_from, path_write_to)
 
